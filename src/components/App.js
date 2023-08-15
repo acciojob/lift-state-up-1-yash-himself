dce512ec828dbadd523dfb3cbdfc9ed1d80d6b92
Yash Gupta <yashgupta27690@gmail.com>
2023-08-15 20:54:38 +0530

    submitting code-Tue Aug 15 2023 20:54:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,27 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [model,setModel] = useState('')
+
+
+function showModel() {
+
+  let a =createElement("h3", { style: { color: "black" } }, 'Model Content',createElement("h5", { style: { color: "black" } }, 'This is the model content'))
+  setModel( a )
+}
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+        <div>
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <div className="child">
+            <h2>Child Component</h2>
+            <button onClick={showModel}>Show Model</button>
+          {model}
+          </div>
+        </div>
+    </div>
     </div>
   )
 }
